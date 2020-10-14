66a51e442b8f4c5f374ad11cc4b8cd2db6f254ed
Sam Ceracas <samceracas@gmail.com>
2020-10-14 21:36:30 +0800

    Updates

---
+++ precache-manifest.1f62057af31cca0a4d152e404204be4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26720c9b35184dd461dd96ec979090d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85f4d510a81c8528aa18",
+    "url": "./static/css/main.2d811b0e.chunk.css"
+  },
+  {
+    "revision": "4a8ff0695a5b3a68dde5",
+    "url": "./static/js/2.c52cd670.chunk.js"
+  },
+  {
+    "revision": "33035800fe77566c3b04b0c4e030cc15",
+    "url": "./static/js/2.c52cd670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f4d510a81c8528aa18",
+    "url": "./static/js/main.fee74c22.chunk.js"
+  },
+  {
+    "revision": "ed0f88ab387210c0c042",
+    "url": "./static/js/runtime-main.1175a32a.js"
+  }
+]);
